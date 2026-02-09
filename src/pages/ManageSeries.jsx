718e7558b2b4a09867e541afe00dd34476e21d18
Cole Catlin <cole@thehijinxco.com>
2026-02-09 17:21:32 +0000

    File changes

--- src/pages/ManageSeries.jsx
+++ src/pages/ManageSeries.jsx
@@ -109,6 +109,10 @@ export default function ManageSeries() {
             setShowForm(false);
             setEditingSeries(null);
           }}
+          onSeriesCreated={(newSeries) => {
+            setShowForm(false);
+            setSelectedSeriesForEdit(newSeries);
+          }}
         />
       </PageShell>
     );
