37e8edb32a9fa8b1cf7c1d0794633f58826ced06
Cole Catlin <cole@thehijinxco.com>
2026-02-10 03:05:10 +0000

    File changes

--- src/components/drivers/DriverCard.jsx
+++ src/components/drivers/DriverCard.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import CountryFlag from '@/components/shared/CountryFlag';
 import { buildProfileUrl } from '@/components/utils/routingContract';
