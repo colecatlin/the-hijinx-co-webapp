a69acc6cf99e8ba9f6f987e11019dc904fd027c6
Cole Catlin <cole@thehijinxco.com>
2026-02-10 21:04:40 +0000

    File changes

--- src/components/management/SeriesForm.jsx
+++ src/components/management/SeriesForm.jsx
@@ -84,12 +84,20 @@ export default function SeriesForm({ series, onClose, onSeriesCreated }) {
                 <SelectValue />
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="Asphalt Oval">Asphalt Oval</SelectItem>
-                <SelectItem value="Road Racing">Road Racing</SelectItem>
+                <SelectItem value="Stock Car">Stock Car</SelectItem>
                 <SelectItem value="Off Road">Off Road</SelectItem>
+                <SelectItem value="Dirt Oval">Dirt Oval</SelectItem>
                 <SelectItem value="Snowmobile">Snowmobile</SelectItem>
-                <SelectItem value="Rallycross">Rallycross</SelectItem>
-                <SelectItem value="Mixed">Mixed</SelectItem>
+                <SelectItem value="Dirt Bike">Dirt Bike</SelectItem>
+                <SelectItem value="Open Wheel">Open Wheel</SelectItem>
+                <SelectItem value="Sports Car">Sports Car</SelectItem>
+                <SelectItem value="Touring Car">Touring Car</SelectItem>
+                <SelectItem value="Rally">Rally</SelectItem>
+                <SelectItem value="Drag">Drag</SelectItem>
+                <SelectItem value="Motorcycle">Motorcycle</SelectItem>
+                <SelectItem value="Karting">Karting</SelectItem>
+                <SelectItem value="Water">Water</SelectItem>
+                <SelectItem value="Alternative">Alternative</SelectItem>
               </SelectContent>
             </Select>
           </div>
