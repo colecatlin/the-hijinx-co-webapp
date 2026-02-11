08aef60bbdde930805b48dce4cbbbd09696ff335
Cole Catlin <cole@thehijinxco.com>
2026-02-11 01:58:32 +0000

    File changes

--- src/pages/DriverProfile.jsx
+++ src/pages/DriverProfile.jsx
@@ -138,7 +138,7 @@ export default function DriverProfile() {
         </Link>
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-12">
-          <div className="lg:col-span-2">
+          <div className="lg:col-span-2 relative -mt-1">
             <div className="flex items-center justify-between mb-2">
               <div className="flex items-center gap-3">
                 <CountryFlag country={driver.hometown_country} />
