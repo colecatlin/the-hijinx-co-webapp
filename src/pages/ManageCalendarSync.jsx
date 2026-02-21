0203d527848834432e24f07429f36ae4bfd76966
Cole Catlin <cole@thehijinxco.com>
2026-02-21 17:10:10 +0000

    File changes

--- src/pages/ManageCalendarSync.jsx
+++ src/pages/ManageCalendarSync.jsx
@@ -101,6 +101,7 @@ export default function ManageCalendarSync() {
                   <Calendar className="w-5 h-5 text-gray-400 shrink-0" />
                   <div className="min-w-0">
                     <div className="font-semibold text-[#232323]">{cal.name}</div>
+                    {cal.seriesName && <div className="text-xs text-blue-500 font-medium">{cal.seriesName}</div>}
                     <div className="text-xs text-gray-400 truncate max-w-sm">{cal.url}</div>
                   </div>
                 </div>
