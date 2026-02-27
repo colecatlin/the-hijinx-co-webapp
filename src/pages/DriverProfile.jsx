19b36f3ee4a9f4384c20af08d2d8f7dd4dab4245
Cole Catlin <cole@thehijinxco.com>
2026-02-27 02:41:20 +0000

    File changes

--- src/pages/DriverProfile.jsx
+++ src/pages/DriverProfile.jsx
@@ -232,9 +232,9 @@ export default function DriverProfile() {
         </div>
       )}
 
-      <div className="max-w-7xl mx-auto px-6 py-12">
+      <div className="max-w-7xl mx-auto px-6 py-2">
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-6 items-start pt-0">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-6 items-start">
           <div className="lg:col-span-2">
             <Separator className="mb-3" />
             <div className="flex items-center justify-between mb-2">
