47eaa743daeb007160aacfbb404476da74d5c73b
Cole Catlin <cole@thehijinxco.com>
2026-02-28 01:48:56 +0000

    File changes

--- functions/importSeasonResults.ts
+++ functions/importSeasonResults.ts
@@ -241,7 +241,7 @@ Deno.serve(async (req) => {
 
       const [classId, eventId, driverId] = await Promise.all([
         seriesId ? getOrCreateClass(seriesId, className) : Promise.resolve(null),
-        getOrCreateEvent(eventName || `${seriesName || 'Race'} - ${eventDate}`, eventDate, trackId, seriesId, season),
+        getOrCreateEvent(eventName || `${seriesName || 'Race'} - ${eventDate}`, eventDate, trackId, seriesId, season, roundNumber),
         getOrCreateDriver(firstName, lastName, bibNumber),
       ]);
 
