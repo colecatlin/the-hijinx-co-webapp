ddd97b69a8061590fa8e5b97ed95a186f82261c4
Cole Catlin <cole@thehijinxco.com>
2026-03-01 06:42:02 +0000

    File changes

--- src/pages/RegistrationDashboard.jsx
+++ src/pages/RegistrationDashboard.jsx
@@ -453,10 +453,10 @@ export default function RegistrationDashboard() {
                   <Upload className="w-4 h-4 mr-1" /> Import CSV
                 </Button>
                 <Button
-                  variant="outline"
-                  size="sm"
-                  disabled={!isAdmin}
-                  onClick={() => setShowSyncModal(true)}
+                   variant="outline"
+                   size="sm"
+                   disabled={!canAdmin}
+                   onClick={() => setShowSyncModal(true)}
                   className="border-gray-700 text-gray-300 hover:bg-gray-800 hover:text-white disabled:opacity-50"
                 >
                   <RefreshCw className="w-4 h-4 mr-1" /> Sync Timing
