18025ee5f2dc71a1f12c0dccf805f4dd6ec7cb24
Cole Catlin <cole@thehijinxco.com>
2026-03-01 06:39:35 +0000

    File changes

--- src/pages/RegistrationDashboard.jsx
+++ src/pages/RegistrationDashboard.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import { base44 } from '@/api/base44Client';
 import { useQuery } from '@tanstack/react-query';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
+import { createPageUrl } from '@/components/utils';
 import PageShell from '@/components/shared/PageShell';
 import BurnoutSpinner from '@/components/shared/BurnoutSpinner';
 import EventBuilderForm from '@/components/management/EventBuilder/EventBuilderForm';
