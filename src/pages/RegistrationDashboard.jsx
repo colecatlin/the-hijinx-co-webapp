dc357ef97b846e68b837084495f8e1b0d24539c8
Cole Catlin <cole@thehijinxco.com>
2026-03-01 05:23:49 +0000

    File changes

--- src/pages/RegistrationDashboard.jsx
+++ src/pages/RegistrationDashboard.jsx
@@ -468,7 +468,8 @@ export default function RegistrationDashboard() {
               </TabsTrigger>
               <TabsTrigger
                 value="entries"
-                className="data-[state=active]:bg-gray-700 data-[state=active]:text-white text-gray-400 px-4 py-2"
+                disabled={!selectedEvent}
+                className="data-[state=active]:bg-gray-700 data-[state=active]:text-white text-gray-400 px-4 py-2 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 <Users className="w-4 h-4 mr-2" /> Entries
               </TabsTrigger>
