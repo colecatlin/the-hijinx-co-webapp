d2cbe37f38287bab9b1ce5bce8e8d7ed322d3a79
Cole Catlin <cole@thehijinxco.com>
2026-03-01 06:41:34 +0000

    File changes

--- src/pages/RegistrationDashboard.jsx
+++ src/pages/RegistrationDashboard.jsx
@@ -444,10 +444,10 @@ export default function RegistrationDashboard() {
                   <Plus className="w-4 h-4 mr-1" /> Create Event
                 </Button>
                 <Button
-                  variant="outline"
-                  size="sm"
-                  disabled={!isAdmin}
-                  onClick={() => setShowImportModal(true)}
+                   variant="outline"
+                   size="sm"
+                   disabled={!canAdmin}
+                   onClick={() => setShowImportModal(true)}
                   className="border-gray-700 text-gray-300 hover:bg-gray-800 hover:text-white disabled:opacity-50"
                 >
                   <Upload className="w-4 h-4 mr-1" /> Import CSV
